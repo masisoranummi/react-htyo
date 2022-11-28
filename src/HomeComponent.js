21169fb15d017e44166fa649297898859b1b87e6
masisoranummi <masi.soranummi@tuni.fi>
2022-11-28 20:40:05 +0200

    feature: add multiple context added

--- src/HomeComponent.js
+++ src/HomeComponent.js
@@ -2,10 +2,11 @@ import { useState, useEffect } from "react";
 
 function HomeComponent() {
     const [current, setCurrent] = useState("");
-    const [time, setTime] = useState("");
+    const [time, setTime] = useState(1);
     const [contexts, setContexts] = useState([]);
     const [newContexts, setNewContexts] = useState([]);
     const [currentcontext, setCurrentContext] = useState([]);
+    const [contextCount, setContextCount] = useState(1);
     const [loading, setLoading] = useState(true);
     console.log(current);
     console.log(time);
@@ -36,16 +37,34 @@ function HomeComponent() {
         setTime(event.target.value);
         event.preventDefault();
     }
-    function handleContext(event) {
-        setCurrentContext(event.target.value);
+    function handleContext(event, index) {
+        let newState = [];
+        for (let i = 0; i < contextCount; i++) {
+            if (i === index) {
+                newState[i] = event.target.value;
+            } else {
+                newState[i] = currentcontext[i];
+            }
+            console.log(index);
+        }
+        setCurrentContext(newState);
+        event.preventDefault();
+    }
+    function handleContextAmount(event) {
+        setContextCount(event.target.value);
         event.preventDefault();
     }
     function handleSubmit() {
-        let currentId = 0;
-        for (let i = 0; i < contexts.length; i++) {
-            if (contexts[i].title === currentcontext) {
-                currentId = contexts[i].id;
+        let currentId = [];
+        for (let j = 0; j < contextCount; j++) {
+            for (let i = 0; i < contexts.length; i++) {
+                if (contexts[i].title === currentcontext[j]) {
+                    currentId[j] = contexts[i].id;
+                }
+                console.log(currentcontext);
+                console.log(contexts.length);
             }
+            console.log(currentId);
         }
         fetch("http://127.0.0.1:3010/posts", {
             method: "POST",
@@ -67,6 +86,15 @@ function HomeComponent() {
                 time
         );
     }
+    let selectContextArray = [];
+    for (let i = 0; i < contextCount; i++) {
+        selectContextArray.push(
+            <select key={i} onChange={(event) => handleContext(event, i)}>
+                value={currentcontext}
+                {newContexts}
+            </select>
+        );
+    }
     return (
         <>
             <h1>Home</h1>
@@ -83,15 +111,24 @@ function HomeComponent() {
                 <label>
                     Hours:
                     <input
+                        className="numberlabel"
                         type="number"
+                        min={1}
                         value={time}
                         onChange={handleTime}
                     />{" "}
                 </label>
-                <select onChange={handleContext}>
-                    value={currentcontext}
-                    {newContexts}
-                </select>
+                <label>
+                    Context amount:
+                    <input
+                        className="numberlabel"
+                        type="number"
+                        min={1}
+                        value={contextCount}
+                        onChange={handleContextAmount}
+                    />{" "}
+                </label>
+                {selectContextArray}
                 <input type="submit" value="Submit" />
             </form>
         </>
