ac5a0721b153ef9ca2cb0ca2b1b58661ce2f4ae4
masisoranummi <masi.soranummi@tuni.fi>
2022-12-05 17:06:53 +0200

    Add stats based on context

--- src/StatsComponent.js
+++ src/StatsComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import StatsContextComponent from "./StatsContextComponent";
 
 class StatsComponent extends React.Component {
     constructor(props) {
@@ -7,18 +8,93 @@ class StatsComponent extends React.Component {
             loading: true,
             stats: [],
             newstats: [],
+            currentContext: "All",
+            allContexts: [<option>{"All"}</option>],
+            fullContexts: [],
+            currentContextId: null,
         };
         this.fetchData = this.fetchData.bind(this);
+        this.newFetch = this.newFetch.bind(this);
+        this.fetchContexts = this.fetchContexts.bind(this);
+        this.handleContext = this.handleContext.bind(this);
+        this.fetchBasedonContext = this.fetchBasedonContext.bind(this);
     }
 
     componentDidMount() {
         if (this.state.loading) {
+            this.fetchContexts();
             fetch("http://localhost:3010/posts")
                 .then((response) => response.json())
                 .then((data) => {
-                    this.setState({ stats: data }, () => this.fetchData());
+                    this.setState({ stats: data }, () => {
+                        if (this.state.currentContext === "All") {
+                            this.fetchData();
+                        } else {
+                            this.fetchBasedonContext();
+                        }
+                    });
                 });
         }
+    }
+
+    newFetch() {
+        if (this.state.currentContext === "All") {
+            this.fetchData();
+        } else {
+            this.fetchBasedonContext();
+        }
+    }
+
+    fetchBasedonContext() {
+        const activityArr = [];
+        for (let i = 0; i < this.state.fullContexts.length; i++) {
+            if (
+                this.state.fullContexts[i].title === this.state.currentContext
+            ) {
+                this.setState(
+                    {
+                        currentContextId: this.state.fullContexts[i].id,
+                    },
+                    () => {
+                        for (let i = 0; i < this.state.stats.length; i++) {
+                            console.log(this.state.stats[i].contextId);
+                            console.log(this.state.currentContextId);
+                            if (
+                                this.state.stats[i].contextId.includes(
+                                    this.state.currentContextId
+                                )
+                            ) {
+                                activityArr.push(
+                                    <p key={i}>
+                                        {this.state.stats[i].activity} for{" "}
+                                        {this.state.stats[i].time} hours
+                                    </p>
+                                );
+                            }
+                        }
+                        this.setState({ loading: false });
+                        console.log(activityArr);
+                        this.setState({ newstats: [...activityArr] });
+                    }
+                );
+            }
+        }
+    }
+
+    fetchContexts() {
+        fetch("http://localhost:3010/contexts")
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ fullContexts: data });
+                data.map((x) =>
+                    this.setState((previousState) => ({
+                        allContexts: [
+                            ...previousState.allContexts,
+                            <option key={x.id}>{x.title}</option>,
+                        ],
+                    }))
+                );
+            });
     }
 
     fetchData() {
@@ -37,11 +113,28 @@ class StatsComponent extends React.Component {
         this.setState({ newstats: [...activityArr] });
     }
 
+    handleContext(event) {
+        this.setState({ currentContext: event.target.value }, () =>
+            this.newFetch()
+        );
+
+        event.preventDefault();
+    }
+
     render() {
         return (
             <>
                 <h1>Stats</h1>
-                <div>{this.state.newstats}</div>
+                <br></br>
+                <select onChange={this.handleContext}>
+                    value={this.state.currentContext}
+                    {this.state.allContexts}
+                </select>
+                {this.state.currentContext === "All" ? (
+                    <div>{this.state.newstats}</div>
+                ) : (
+                    <div>{this.state.newstats}</div>
+                )}
             </>
         );
     }
