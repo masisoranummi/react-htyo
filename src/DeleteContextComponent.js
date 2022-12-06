af3283409035c349990c22b449e07a5c2e209650
masisoranummi <masi.soranummi@tuni.fi>
2022-12-06 14:56:40 +0200

    improve context deleting

--- src/DeleteContextComponent.js
+++ src/DeleteContextComponent.js
@@ -4,6 +4,7 @@ function DeleteContext() {
     const [contexts, setContexts] = useState([]);
     const [currentcontext, setCurrentContext] = useState();
     const [newContexts, setNewContexts] = useState([]);
+    const [targetActivities, setTargetActivities] = useState([]);
     const [loading, setLoading] = useState(true);
     const [open, setOpen] = useState(false);
     useEffect(() => {
@@ -20,17 +21,49 @@ function DeleteContext() {
                                 {data[i].title}
                             </option>,
                         ]);
-                        console.log(data[i].title);
                     }
                 }
             });
     }, [loading]);
 
+    useEffect(() => {
+        const fetchdata = async () => {
+            let currentId = 1;
+            for (let i = 0; i < contexts.length; i++) {
+                if (contexts[i].title === currentcontext) {
+                    currentId = contexts[i].id;
+                }
+            }
+            let res = await fetch("http://localhost:3010/posts");
+            let jsonres = await res.json();
+            console.log(jsonres);
+            console.log(currentId);
+            console.log(currentcontext);
+            let targetArray = [];
+            for (let i = 0; i < jsonres.length; i++) {
+                if (jsonres[i].contextId.includes(currentId)) {
+                    jsonres[i].contextId = jsonres[i].contextId.filter(
+                        (e) => e !== currentId
+                    );
+                    targetArray.push(jsonres[i]);
+                }
+            }
+            setTargetActivities(targetArray);
+        };
+        fetchdata();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentcontext]);
+
+    useEffect(() => {
+        console.log(targetActivities);
+    }, [targetActivities]);
+
     const handleOpen = () => {
         setOpen(!open);
     };
-    function handleContext(event) {
+    async function handleContext(event) {
         setCurrentContext(event.target.value);
+
         event.preventDefault();
     }
     function handleSubmit() {
@@ -44,6 +77,21 @@ function DeleteContext() {
             }
             console.log(currentId);
             console.log(currentcontext);
+        }
+        for (let i = 0; i < targetActivities.length; i++) {
+            fetch(`http://127.0.0.1:3010/posts/${targetActivities[i].id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    activity: targetActivities[i].activity,
+                    time: targetActivities[i].time,
+                    contextId: targetActivities[i].contextId,
+                }),
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log("deleted context from " + data.activity);
+                });
         }
         fetch(`http://127.0.0.1:3010/contexts/${currentId}`, {
             method: "DELETE",
